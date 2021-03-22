70f8e29a2f9a58b99d367436413233483885c154
William-Moreno <intrcptr@comcast.net>
2021-03-22 16:56:14 -0700

    basic component folders and files setup

---
+++ src/components/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './main.scss';
+
+function Main() {
+  return (
+    <footer className="App-main">
+      <p>Main Body</p>
+    </footer>
+  )
+}
+
+export default Main;
